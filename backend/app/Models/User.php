d6bd69de215466d77048729c856f5b426ddde6f5
minisera <koki.ohtaka0821@gmail.com>
2021-05-05 19:04:30 +0900

    PostController作成、ルーティング作成

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Fortify\TwoFactorAuthenticatable; 
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::prefix('post')->group(function() {
+    Route::get('/index','App\Http\Controllers\PostController@index')->name('posts.index');
+});
