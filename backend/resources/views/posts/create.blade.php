609c59487f3d2ab5a8af0e45c6186660ea722c69
minisera <koki.ohtaka0821@gmail.com>
2021-06-05 19:35:35 +0900

    jsを使ってformの行き先を変更する

--- backend/resources/views/posts/create.blade.php
+++ backend/resources/views/posts/create.blade.php
@@ -1,18 +1,29 @@
 PostCreate
 <p>{{$user->name}}</p>
-{{ Form::open(['route' => 'posts.session', 'method' => 'post']) }}
+{{ Form::open(['route' => 'posts.store', 'method' => 'post', 'id' => 'post_form']) }}
   <div>タイトル</div>
   {{ Form::text('title',$session['title']?? '')}}
   <div>内容</div>
   {{ Form::text('text',$session['text']?? '')}}
   <div>添付article</div>
   {{ Form::select('article_id',$articles,$session['article_id']?? '')}}
-  <a href="{{route('articles.create')}}">article新規作成画面へ</a>
   <div>{{ Form::text('url',url()->current())}}</div>
   <pre>
+    {{ Form::submit('articleを新規作成する',['id' => 'put_post_session'])}}
     {{ Form::submit('登録')}}
   </pre>
 {{ Form::close() }}
 <pre>
 <a href="{{route('posts.index')}}">やっぱやめる</a>
 </pre>
+
+<script>
+  let putPostSession = document.getElementById('put_post_session');
+  let postForm = document.getElementById('post_form');
+  putPostSession.addEventListener('mouseover',function(){
+    postForm.setAttribute('action',"{{route('posts.session')}}")
+  });
+  putPostSession.addEventListener('mouseout',function(){
+    postForm.setAttribute('action',"{{route('posts.store')}}")
+  });
+</script>
